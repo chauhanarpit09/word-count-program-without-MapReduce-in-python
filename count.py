52c74c5b196baf4acc5972d91674835e7eaa3102
Arpit Chauhan <arpit09chauhan@gmail.com>
2020-07-26 16:05:33 +0530

    haddop

---
+++ count.py
@@ -0,0 +1,20 @@
+#%%
+import re
+file = open("file.txt","r")
+
+l = file.read()
+l = re.sub("[:,.?()#$]","",l).split()
+
+d = {}
+for i in l:
+    if i in d.keys():
+        d[i] = d[i]+1
+    else:
+        d[i] = 1
+for k,v in d.items():
+    print(k , " ",v," times" )
+
+file.close()
+
+
+# %%
